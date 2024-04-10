94a2f719b6f83af6debc7e8fa695763dcf090700
Alexey Sirko <leshasirko@gmail.com>
2024-04-10 14:51:39 +0300

    Added dynamic control test

--- ExampleProject/Selenium/DynamicControlsTests.cs
+++ ExampleProject/Selenium/DynamicControlsTests.cs
@@ -1,5 +1,8 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestPlatform.ObjectModel;
+using NUnit.Framework;
 using OpenQA.Selenium;
+using SeleniumExtras.WaitHelpers;
+using System;
 
 namespace ExampleProject.Selenium
 {
@@ -7,15 +10,31 @@ namespace ExampleProject.Selenium
     {
         private static readonly By dynamicControl = By.XPath(string.Format(preciseTextXpath, "Dynamic Controls"));
         private static readonly By enableBtn = By.XPath(string.Format(preciseTextXpath, "Enable"));
+        private static readonly By inputField = By.XPath("//*[@id='input-example']//input");
+        private static readonly string randomValue = Guid.NewGuid().ToString();
 
         [Test]
         public void DynamicControlsTest()
         {
             driver.FindElement(dynamicControl).Click();
             driver.FindElement(enableBtn).Click();
-            //assert input is enabled
-            //input randomly generated text
-            //assert input text
+            var inputFieldElement = driver.FindElement(inputField);
+            Assert.That(IsEnabled(inputFieldElement), "imput is not enabled");
+            inputFieldElement.SendKeys(randomValue);
+            Assert.That(driver.FindElement(inputField).GetAttribute("value"), Is.EqualTo(randomValue), "Text is not displayed");
+        }
+
+        private bool IsEnabled(IWebElement element)
+        {
+            try
+            {
+                wait.Until(ExpectedConditions.ElementToBeClickable(element));
+            }
+            catch (TimeoutException)
+            {
+                return false;
+            }
+            return true;
         }
     }
 }
