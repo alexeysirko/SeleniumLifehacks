1011f28a2277c9e055d1aca9f59d40f86d8e9cb3
Alexey Sirko <leshasirko@gmail.com>
2024-04-10 17:57:09 +0300

    Finished iframe test

--- ExampleProject/Selenium/IFrameTests.cs
+++ ExampleProject/Selenium/IFrameTests.cs
@@ -10,22 +10,31 @@ namespace ExampleProject.Selenium
         private static readonly By iframeBtn = By.XPath(string.Format(preciseTextXpath, "iFrame"));
         private static readonly By editBtn = By.XPath(string.Format(preciseTextXpath, "Edit"));
         private static readonly By undoBtn = By.XPath(string.Format(preciseTextXpath, "Undo"));
+        private static readonly By textField = By.Id("tinymce");
+        
         private static readonly string textIsNotDisplayedMsg = "Text is not displayed";
         private static readonly string randomValue = Guid.NewGuid().ToString();
         private static readonly string initText = "Your content goes here.";
+        private static readonly string iFrameId = "mce_0_ifr";
 
         [Test]
         public void IFrameTest()
         {
             driver.FindElement(framesBtn).Click();
             driver.FindElement(iframeBtn).Click();
-            //input text to the textfield
+            driver.SwitchTo().Frame(iFrameId);
+
+            driver.FindElement(textField).SendKeys(randomValue);
             Assert.That(driver.FindElement(By.XPath(string.Format(preciseTextXpath, initText + randomValue))).Displayed,
                     textIsNotDisplayedMsg);
 
+            driver.SwitchTo().DefaultContent();
             driver.FindElement(editBtn).Click();
             driver.FindElement(undoBtn).Click();
-            //assert text is not displayed
+
+            driver.SwitchTo().Frame(iFrameId);
+            Assert.That(driver.FindElement(By.XPath(string.Format(preciseTextXpath, initText))).Displayed, textIsNotDisplayedMsg);
+
         }
     }
 }
