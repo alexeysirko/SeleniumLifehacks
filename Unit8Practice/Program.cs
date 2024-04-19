edf186b0119c04636494a35be1452f8c1ce3e487
Alexey Sirko <leshasirko@gmail.com>
2024-04-19 13:13:57 +0300

    Finished v1

--- Unit8Practice/Program.cs
+++ Unit8Practice/Program.cs
@@ -9,6 +9,21 @@ animals.Add(new Donkey(12, 69, "Donny"));
 animals.Add(new Monkey(18, 75, "Bamboo"));
 animals.Add(new Donkey(2, 32.2, "Little"));
 
-Zoo zoo = new(animals, "NewYork");
+Zoo zoo = new(animals, "New York");
 zoo.PrintAnimals();
+Console.WriteLine();
 zoo.FeedAnimals();
+
+Console.WriteLine();
+List<Animal> filteredAnimals = AnimalsFilter.FilterByAge(animals, 10);
+Zoo filteredZoo = new(filteredAnimals, "City of grown-up animals");
+filteredZoo.PrintAnimals();
+
+List<Zoo> zoos = new();
+zoos.Add(zoo);
+zoos.Add(filteredZoo);
+Console.WriteLine("Practice with lambda");
+zoos
+    .Where(zoo => zoo.GetCity().Contains("grown-up"))
+    .ToList()
+    .ForEach(filteredZoo => filteredZoo.PrintAnimals());
