af4d50bcd4f3bea5559a6006b69107d22b854ffa
Alexey Sirko <leshasirko@gmail.com>
2024-06-24 19:28:18 +0300

    Impemented file upload and fixed path for download

---
+++ ExampleProject/Framework/Pages/FileUploadPage.cs
@@ -0,0 +1,31 @@
+﻿using Aquality.Selenium.Elements.Interfaces;
+using Aquality.Selenium.Forms;
+using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExampleProject.Framework.Pages
+{
+    internal class FileUploadPage : Form
+    {
+        private const string PageName = "File Upload";
+        private readonly IButton fileUploadBtn = ElementFactory.GetButton(By.XPath(string.Format(LocatorConstants.PreciseTextLocator, "File Upload")), "File Upload");
+        private readonly ITextBox chooseFileTxb = ElementFactory.GetTextBox(By.Id("file-upload"), "Choose file button");
+        private readonly IButton fileSubmitBtn = ElementFactory.GetButton(By.Id("file-submit"), "Submit file field");
+        private readonly ILabel uploadedFileField = ElementFactory.GetLabel(By.Id("uploaded-files"), "Uploaded file field");
+
+
+        public FileUploadPage() : base(By.XPath(string.Format(LocatorConstants.PreciseTextLocator, PageName)), PageName)
+        {
+        }
+
+
+        public void ClickFileUploadBtn() => fileUploadBtn.Click();
+        public void SendKeysToFileToChooseFileTxb(string value) => chooseFileTxb.SendKeys(value);
+        public void ClickFileSubmitBtn() => fileSubmitBtn.Click();
+        public string GetNameOfUploadedFile() => uploadedFileField.Text;
+    }
+}
--- ExampleProject/Framework/Tests/FileUploadTests.cs
+++ ExampleProject/Framework/Tests/FileUploadTests.cs
@@ -1,8 +1,23 @@
-﻿namespace ExampleProject.Framework.Tests
+﻿using ExampleProject.Framework.Pages;
+using NUnit.Framework;
+using System.IO;
+
+namespace ExampleProject.Framework.Tests
 {
     internal class FileUploadTests : BaseTest
     {
+        private static readonly FileUploadPage fileUploadPage = new();
+
         private static readonly string fileName = testdata.GetValue<string>("fileUpload.fileName");
-        private static readonly string filePath = testdata.GetValue<string>("fileUpload.filePath") + fileName;
+        private static readonly string filePath = testdata.GetValue<string>("fileUpload.folderPath") + fileName;
+
+        [Test]
+        public void FileUploadTest()
+        {
+            fileUploadPage.ClickFileUploadBtn();
+            fileUploadPage.SendKeysToFileToChooseFileTxb(Path.GetFullPath(filePath));
+            fileUploadPage.ClickFileSubmitBtn();
+            Assert.That(fileUploadPage.GetNameOfUploadedFile(), Is.EqualTo(fileName));
+        }
     }
 }
